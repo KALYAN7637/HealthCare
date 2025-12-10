f0c9c2d9d53bb796313a05db21e68e24ec77ceb2
NALAMKALYANACHAKRAVARTHI <100521699+KALYAN7637@users.noreply.github.com>
2025-12-10 12:07:10 +0530

    Add files via upload

---
+++ configs.py
@@ -0,0 +1,4 @@
+DATAOPS_CLIENT_ID = "dataopssuite-restapi-client"
+DATAOPS_CLIENT_SECRET = "hhTJgx#t"
+DATAOPS_USERNAME = "kalyanachakravarthi.nalam"
+DATAOPS_PASSWORD = "U2FsdGVkX18gYIzb2S9jHqhtJmjDXDmO6V6YXCHQj70="
